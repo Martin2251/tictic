98e1d9fdd7e0c379acd6ac316222a532ad3b3252
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-07-05 08:24:11 +0100

    added classname

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ import Logo from "/utils/tiktik-logo.png"
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='w-full flex justify-between items-center border-b-2'>
       <Link href="/">
       <div>
           <Image 
